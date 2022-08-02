6e13c5e41e1d56e863e3f2e03b877b46468f1924
ed-nant <110224817+ed-nant@users.noreply.github.com>
2022-08-02 20:47:27 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<h2> Selamat berlatih kolaborator </h2>";
-echo "<h3> Isi Nama dan Nim Anda dibawah baris code ini </h3>";
-<<<<<<< HEAD
-echo "<h3>Ng. edi ananto (165610115)</h3>"
-=======
->>>>>>> upstream/master
-?> 
